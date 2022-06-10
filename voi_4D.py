ea26e8cf01c62f076ad4b84eb58ae88ba4c341a7
syxz0628 <syxz0628@163.com>
2022-06-10 14:28:19 +0200

    first 0.1

--- voi_4D.py
+++ voi_4D.py
@@ -1,5 +1,4 @@
 import os
-
 class class_gen_4D_vois():
     def __init__(self,patinfo):
         self.patinfo=patinfo
@@ -30,7 +29,7 @@ class class_gen_4D_vois():
                 read_trafo= 'trafo \''+data_path+'Reg/'+str(self.patinfo.patientName[patientNo])+'\' / r select(bw)'
                 read_3Dvoi= 'voi \''+data_path+'vois/3D/Average/'+str(self.patinfo.patientName[patientNo])+'\' / r'
                 voisine2='voi * / create4D voistate(0) maxthreads(16)'
-                write_4Dvoi='voi / write bin prefix(\''+data_path+'vois/4D/'
+                write_4Dvoi='voi / write bin prefix(\''+data_path+'vois/4D/\')'
                 print(read_trafo)
                 print(read_3Dvoi)
                 print(voisine2)
@@ -43,10 +42,9 @@ class class_gen_4D_vois():
                 with open(createfilepath,'w+') as writeexec:
                     writeexec.writelines(
                         read_trafo + os.linesep + read_3Dvoi + os.linesep + voisine2 + os.linesep + write_4Dvoi + os.linesep + 'quit')
-                cdtofolder= 'cd '+data_path+'/vois/'+os.linesep
+                cdtofolder= 'cd '+data_path+'vois/'+os.linesep
                 runtrip='runtrip.sh create4Dvois.exec'+os.linesep
                 cdoutfolder='cd ../../../'+os.linesep
                 printinfo='#finished '+self.patinfo.patientName[patientNo]+os.linesep+os.linesep
-                writesh.writelines(cdtofolder+runtrip+printinfo+cdoutfolder)
-
+                writesh.writelines(cdtofolder+runtrip+cdoutfolder+printinfo)
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
