b74cf112e42e560e6721fdb64b40266a31344d8f
syxz0628 <syxz0628@163.com>
2022-06-14 21:28:36 +0200

    first 0.1

--- reg_4DCT.py
+++ reg_4DCT.py
@@ -7,17 +7,21 @@ class class_reg_4DCT():
     def fun_auto_reg_4DCT(self):
         print("start registrate 4D CTs for all patients listed in patinfo.txt")
         print(self.patinfo.patientName)
-        for patientNo in range(0,len(self.patinfo.patientName)):
-            print("#trying plastimatch in: ",self.patinfo.patientName[patientNo])
-            FDctdir='/d/bio/medphys/PatienData/SPHIC_motion_mitigate/' + str(self.patinfo.patientID[patientNo]) + '/' + \
-                        str(self.patinfo.ctName[patientNo]) + '/' + 'ctx/'
-            refCTname = FDctdir + str(self.patinfo.patientName[patientNo]) + '_00.nrrd'
-            outputname='./' + str(self.patinfo.patientID[patientNo]) + '/' + \
-                        str(self.patinfo.ctName[patientNo]) + '/' + 'Reg/'
-            execommand = 'python /u/motion/Software/RegistrationScript/createPlastimatchScript.py F ' + refCTname + ' -D ' + FDctdir + ' -O ' + outputname + ' -n ' + \
-                         str(self.patinfo.patientName[patientNo]) + ' -t bspline -S -p 32'
-            rmcommand='rm -rf '+outputname+'*'
-            print(rmcommand)
-            print(execommand)
-            #tmp = os.popen(execommand).readlines()
-            print("#finished plastimatch in: ",self.patinfo.patientName[patientNo])
+        if self.patinfo.rfind('/')!=-1:
+            regshfile=self.patinfo[:self.patinfo.rfind('/')]+'01_CreatePlastimatch4D.sh'
+        else:
+            regshfile = './01_CreatePlastimatch4D.sh'
+        with open(regshfile) as createplastimath:
+            for patientNo in range(0,len(self.patinfo.patientName)):
+                createplastimath.writelines("#trying plastimatch in: "+self.patinfo.patientName[patientNo]+os.linesep)
+                FDctdir='/d/bio/medphys/PatienData/SPHIC_motion_mitigate/' + str(self.patinfo.patientID[patientNo]) + '/' + \
+                            str(self.patinfo.ctName[patientNo]) + '/' + 'ctx/'
+                refCTname = FDctdir + str(self.patinfo.patientName[patientNo]) + '_00.nrrd'
+                outputname='./' + str(self.patinfo.patientID[patientNo]) + '/' + \
+                            str(self.patinfo.ctName[patientNo]) + '/' + 'Reg/'
+                execommand = 'python /u/motion/Software/RegistrationScript/createPlastimatchScript.py F ' + refCTname + ' -D ' + FDctdir + ' -O ' + outputname + ' -n ' + \
+                             str(self.patinfo.patientName[patientNo]) + ' -t bspline -S -p 32'
+                rmcommand='rm -rf '+outputname+'*'
+                createplastimath.writelines(rmcommand+os.linesep+execommand+os.linesep)
+                #tmp = os.popen(execommand).readlines()
+                createplastimath.writelines("#finished plastimatch for: "+self.patinfo.patientName[patientNo]+os.linesep+os.linesep)
