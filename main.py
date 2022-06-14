3ed5369127661f28f30fd0776c11069cb5bba325
syxz0628 <syxz0628@163.com>
2022-06-14 20:56:19 +0200

    first 0.1

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
         voi_3D=voi_ave2p00.class_gen_ave_00_vois(CTinfo)
         voi_3D.fun_preparefile()
         voi_3D.fun_gen_ph00_vois_exec()
-    if args.motioninfo!=None:
+    if args.motionpath!=None:
         dafmbrdata=read_motion_info.class_readmotion_info(args.motionpath)
         dafmbrdata.fun_readpat_motion_info()
         creatlmdoutsh=dafmbr_lmdout.class_dafmbr_lmdout_script(CTinfo,dafmbrdata)
