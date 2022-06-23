508a9ef280c2e2e38e7ec508513cd150a013a4d8
syxz0628 <syxz0628@163.com>
2022-06-23 19:10:38 +0200

    first 0.1

--- temp_write_planinfo.py
+++ temp_write_planinfo.py
@@ -27,12 +27,19 @@ class class_temp():
                 rmfolder = 'rm /u/ysheng/MyAIXd/projects/patients/'
                 Tddose = self.motioninfo.patientID[tempNumber] + '/3Ddose/dose/'
                 Fddose = self.motioninfo.patientID[tempNumber] + '/4DdoseRecon/dose/'
+                Tdexec = self.motioninfo.patientID[tempNumber] + '/3Ddose/exec/'
+                Fdexec = self.motioninfo.patientID[tempNumber] + '/4DdoseRecon/exec/'
                 rmcommand3D = rmfolder + Tddose + '* -rf'
                 rmcommand4D = rmfolder + Fddose + '* -rf'
                 mkdir3Ddose = patientpath + Tddose
                 mkdir43Ddose = patientpath + Fddose
+                mkdir3Dexec = patientpath + Tdexec
+                mkdir43Dexec = patientpath + Fdexec
                 mkdir3D = patientpath + Tddose + self.motioninfo.planName[tempNumber]
                 mkdir4D = patientpath + Fddose + self.motioninfo.planName[tempNumber]
+                mkdir3D2 = patientpath + Tdexec + self.motioninfo.planName[tempNumber]
+                mkdir4D2 = patientpath + Fdexec + self.motioninfo.planName[tempNumber]
                 writesh.writelines(
-                     mkdir3Ddose + os.linesep + mkdir43Ddose + os.linesep + mkdir3D + os.linesep + mkdir4D + os.linesep)
-
+                     mkdir3Ddose + os.linesep + mkdir43Ddose + os.linesep + mkdir3D + os.linesep + mkdir4D + os.linesep+os.linesep)
+                writesh.writelines(
+                     mkdir3Dexec + os.linesep + mkdir43Dexec + os.linesep + mkdir3D2 + os.linesep + mkdir4D2 + os.linesep)
